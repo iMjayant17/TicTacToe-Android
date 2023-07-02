3430f1dadfa87c354c2a67fa90959bf102ef9dbb
jaYanT <jayant74242@gmail.com>
2023-07-03 05:10:43 +0530

    TicTacToe game made solely for the purpose of practice $jaYanT$

--- app/src/main/java/com/example/tictactoe/MainActivity.kt
+++ app/src/main/java/com/example/tictactoe/MainActivity.kt
@@ -3,7 +3,6 @@ package com.example.tictactoe
 import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Handler
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
